102ac242f35dcce8381753363a6b14f1a213563b
soniwill <willianjefferson7@gmail.com>
2021-12-24 11:54:43 -0300

    Uploading more project files and updating the containerTest file.

--- Assets/Tests/EditMode/ContainerTest.cs
+++ Assets/Tests/EditMode/ContainerTest.cs
@@ -9,21 +9,21 @@ public class ContainerTest
 {
     // A Test behaves as an ordinary method
     [Test]
-    public void ContainerShouldContainElement(Bounds elementBounds)
+    public void ContainerShouldContainElement(Bounds elementBounds, Container container)//Todo this is not the correct container class. It still need to be created. the current one is form system.component model name space.
     {
         // Use the Assert class to test conditions
     }
     
     [Test]
-    public void ContainerShouldNotContainElement(Bounds elementBounds)
+    public void ContainerShouldNotContainElement(Bounds elementBounds, Container container)//Todo this is not the correct container class. It still need to be created. the current one is form system.component model name space.
     {
         // Use the Assert class to test conditions
     }
-    
+
     [Test]
-    public void ContainerShouldContainExpecetedNumberOfCarousels(int expectedCarouselNumber, Container container)
+    public void ContainerShouldContainExpecetedNumberOfCarousels(int expectedCarouselNumber, Container container) //Todo this is not the correct container class. It still need to be created. the current one is form system.component model name space.
     {
-        // Use the Assert class to test conditions
+        // container should be passed need to check if how many carousel it has.
     }
 
     
